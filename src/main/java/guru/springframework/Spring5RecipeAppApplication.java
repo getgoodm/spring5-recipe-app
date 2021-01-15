4e434574814560a924c7a31e861684b126a022d0
getgoodm <1481vol@gmail.com>
2021-01-15 22:28:32 +0300

    Attempted to fix jaxb issue

--- src/main/java/guru/springframework/Spring5RecipeAppApplication.java
+++ src/main/java/guru/springframework/Spring5RecipeAppApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Spring5RecipeAppApplication {
 
 	public static void main(String[] args) {
+		System.setProperty("javax.xml.bind.JAXBContextFactory", "com.sun.xml.bind.v2.ContextFactory");
+
 		SpringApplication.run(Spring5RecipeAppApplication.class, args);
 	}
 }
