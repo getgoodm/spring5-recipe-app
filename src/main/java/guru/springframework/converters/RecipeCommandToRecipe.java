cc23b823e70f04e3bf55d7fab3237b8b2434d809
getgoodm <1481vol@gmail.com>
2021-01-19 23:05:48 +0300

    Added command objects and converters

---
+++ src/main/java/guru/springframework/converters/RecipeCommandToRecipe.java
@@ -0,0 +1,57 @@
+package guru.springframework.converters;
+
+import guru.springframework.commands.CategoryCommand;
+import guru.springframework.commands.IngredientCommand;
+import guru.springframework.commands.RecipeCommand;
+import guru.springframework.domain.Category;
+import guru.springframework.domain.Recipe;
+import lombok.Synchronized;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.lang.Nullable;
+import org.springframework.stereotype.Component;
+
+@Component
+public class RecipeCommandToRecipe implements Converter<RecipeCommand, Recipe> {
+
+    private final CategoryCommandToCategory categoryConverter;
+    private final IngredientCommandToIngredient ingredientConverter;
+    private final NotesCommandToNotes notesConverter;
+
+    public RecipeCommandToRecipe(CategoryCommandToCategory categoryConveter, IngredientCommandToIngredient ingredientConverter,
+                                 NotesCommandToNotes notesConverter) {
+        this.categoryConverter = categoryConveter;
+        this.ingredientConverter = ingredientConverter;
+        this.notesConverter = notesConverter;
+    }
+
+    @Synchronized
+    @Nullable
+    @Override
+    public Recipe convert(RecipeCommand source) {
+        if (source == null) return null;
+
+        final Recipe recipe = new Recipe();
+        recipe.setId(source.getId());
+        recipe.setCookTime(source.getCookTime());
+        recipe.setPrepTime(source.getPrepTime());
+        recipe.setDescription(source.getDescription());
+        recipe.setDifficulty(source.getDifficulty());
+        recipe.setDirections(source.getDirections());
+        recipe.setServings(source.getServings());
+        recipe.setSource(source.getSource());
+        recipe.setUrl(source.getUrl());
+        recipe.setNotes(notesConverter.convert(source.getNotes()));
+
+        if (source.getCategories() != null && source.getCategories().size() > 0){
+           for (CategoryCommand category : source.getCategories())
+               recipe.getCategories().add(categoryConverter.convert(category));
+        }
+
+        if (source.getIngredients() != null && source.getIngredients().size() > 0){
+            for (IngredientCommand ingredient : source.getIngredients())
+                recipe.getIngredients().add(ingredientConverter.convert(ingredient));
+        }
+
+        return recipe;
+    }
+}
---
+++ src/main/java/guru/springframework/converters/UomToUomCommand.java
@@ -0,0 +1,27 @@
+package guru.springframework.converters;
+
+import guru.springframework.commands.UnitOfMeasureCommand;
+import guru.springframework.domain.UnitOfMeasure;
+import jdk.internal.jline.internal.Nullable;
+import lombok.Synchronized;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UomToUomCommand implements Converter<UnitOfMeasure, UnitOfMeasureCommand> {
+
+    @Synchronized
+    @Nullable
+    @Override
+    public UnitOfMeasureCommand convert(UnitOfMeasure source) {
+
+        if (source != null) {
+            final UnitOfMeasureCommand uomc = new UnitOfMeasureCommand();
+            uomc.setId(source.getId());
+            uomc.setDescription(source.getUom());
+            return uomc;
+        }
+        return null;
+
+    }
+}
